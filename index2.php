8cc5abc1c2a557017665f7e029ffbdfbb773daf7
ivar2kke <ivarakke15@gmail.com>
2016-05-02 14:39:36 +0300

    mysql stuff

--- index2.php
+++ index2.php
@@ -2,9 +2,25 @@
 
 require "vendor/autoload.php";
 
+use Illuminate\Database\Capsule\Manager as DB;
+
+$db = new DB;
+$db->addConnection([
+    'driver'    => 'mysql',
+    'database'  => 'consoles',
+    'username'  => 'root',
+    'password'  => '',
+    'host'      => 'localhost',
+    'charset'   => 'latin1',
+    'collation' => 'latin1_swedish_ci',
+    'prefix'    => ''
+]);
+$db->setAsGlobal();
+
 $consoles = [
-	"   Playstation 3   ",
-	" Xbox 360 ",
-	"     Nintendo Wii "
+	["name" => "Playstation 3   ", "image" => "https://images-na.ssl-images-amazon.com/images/G/01/videogames/detail-page/B004XABXY0.04.lg.jpg"],
+	["name" => " Xbox 360 ", "image" => "https://upload.wikimedia.org/wikipedia/commons/thumb/6/67/Microsoft-Xbox-360-E-wController.jpg/220px-Microsoft-Xbox-360-E-wController.jpg"],
+	["name" => "     Nintendo Wii ", "image" => "https://tctechcrunch2011.files.wordpress.com/2013/01/nintendowii.gif?w=440&h=330&crop=1"]
 ];
 
+?>
